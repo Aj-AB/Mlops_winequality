fb4839fd9fc70404565e151dd23fa74c297ebdf1
Aj-AB <mr.baraily12@gmail.com>
2022-05-26 21:52:10 +0530

    conftest update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
 import pytest
-import yaml
+from ruamel.yaml import yaml
 import os
 import json
 
 
 @pytest.fixture
 def config(config_path="params.yaml"):
-    with open(os.path.join(os.path.dirname(config_path))) as yaml_file:
+    with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
 
 
 @pytest.fixture
 def schema_in(schema_path="schema_in.json"):
-    with open(os.path.join(os.path.dirname(schema_path))) as json_file:
+    with open(schema_path) as json_file:
         schema = json.load(json_file)
     return schema
